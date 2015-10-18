62b599b907d67fae977ab6fe5e1d692f507eb502
David Navarro Alvarez <davengeo@yahoo.es>
2015-10-18 19:53:10 +0200

    better with lamdba

--- src/main/java/org/daven/demo/rxcache/EventController.java
+++ src/main/java/org/daven/demo/rxcache/EventController.java
@@ -18,44 +18,33 @@ import rx.schedulers.Schedulers;
 
 import java.util.EventListener;
 
-import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.http.ResponseEntity.accepted;
 
 @RestController
 @RequestMapping("/api/event")
 public class EventController {
 
     private static Logger LOG = LoggerFactory.getLogger(EventController.class);
-
-    @RequestMapping(method = RequestMethod.POST)
-    public HttpEntity<String> postEvent(
-            @RequestParam(value = "event",required = true) String event) {
-        LOG.info("received {}", event);
-        emitListener.emit(event);
-        return ok().body("OK");
-    }
+    private static EmitListener emitListener;
+    private static Observable<String> stream =
+            Observable.create((Subscriber<? super String> subscriber) -> {
+                register(subscriber::onNext);
+            }).subscribeOn(Schedulers.io());
 
     public static Observable<String> getStream() {
         return stream;
     }
 
-    private static Observable<String> stream =
-            Observable.create((Subscriber<? super String> subscriber) -> {
-
-        class RestListener implements  EmitListener {
-
-            @Override
-            public void emit(String str) {
-                subscriber.onNext(str);
-            }
-        }
-        register(new RestListener());
-    }).
-            subscribeOn(Schedulers.io());
-
-    private static EmitListener emitListener;
-
     private static void register(EmitListener listener) {
         emitListener = listener;
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public HttpEntity<String> postEvent(
+            @RequestParam(value = "event", required = true) String event) {
+        LOG.info("received {}", event);
+        emitListener.emit(event);
+        return accepted().body("OK");
     }
 
     interface EmitListener extends EventListener {
